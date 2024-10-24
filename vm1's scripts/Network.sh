0ae00cde9c1bdbacf83f17d7c33f71643481a6d3
imabf <abfmashhadani@gmail.com>
2024-10-24 23:40:36 +0300

    initial Network.sh script (not completed)

---
+++ vm1's scripts/Network.sh
@@ -0,0 +1,28 @@
+log_file="network.log" > "$log_file"
+serverIP=$(hostname -I | awk '{print $1}') #save the ip address of the server (the one who ran the script)
+if [ "$#" -ne 2 ]; then # if statment to make sure there are 2 arguments
+	echo Invalid arguemnets
+	exit 1
+fi
+test_ping(){
+	local client_IP=$1 # save the arguemnt passed through the call in line 27
+	local count=0
+	while [ $count -lt 3 ]; do
+		if ping -c 1 -W 3 "$target_IP" > /dev/null; then
+			currentDate=$(date +"%Y-%m-%d %H:%M:%S")
+			ping -c 1 -W 3 "$target_IP"
+
+			echo "$currentDate: ping with $client_IP is working successfully" | tee -a "$log_file"
+			
+			return 0
+		else 
+			echo "ping with $client_IP is not working" | tee -a "$log_file"
+			./traceroute.sh "$client_IP" | tee -a "$log_file"    #here call the traceroute ( still didnt do the traceroute )
+		fi	
+		count=$((count+1))
+	done
+
+}
+for target_IP in "$@"; do
+	test_ping "$target_IP" # loop to go through the arguments 
+done
