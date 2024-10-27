c18cc0a3851a2a3f73828488ce449a237e3c6f89
imabf <abfmashhadani@gmail.com>
2024-10-27 21:40:15 +0300

    completed System.sh script

--- vm1's scripts/System.sh
+++ vm1's scripts/System.sh
@@ -1,15 +1,13 @@
-diskLog="disk_info.log"
-memCpuLog="mem_cpu_int.log"
-> "$disk_log"
-> "$mem_cpu_log"
-du -h $HOME | tee -a "$diskLog"
-df -h $HOME | tee -a "$diskLog"
+diskLog="disk_info.log" > "$diskLog"
+memCpuLog="mem_cpu_int.log" > "$memCpuLog"
+df -h ~ >> $diskLog
 memorytotal=$(free | grep Mem | awk '{print $2}')
 memoryused=$(free | grep Mem | awk '{print $3}')
 memoryfree=$(free | grep Mem | awk '{print $4}')
-echo "Memory Usage:" | tee -a "$memCpuLog"
-echo "The precentage of the total memory used is: $((memoryused*100/memorytotal))%" | tee -a "$memCpuLog"
-echo "The precentage of the total free memory space is: $((memoryfree*100/memorytotal))%" | tee -a "$memCpuLog"
+echo "Memory Usage:" >> $memCpuLog
+echo "The precentage of the total memory used is: $((memoryused*100/memorytotal))%" >> $memCpuLog
+echo "The precentage of the total free memory space is: $((memoryfree*100/memorytotal))%" >>$memCpuLog
  #the file that have the cpu model name is /proc/cpouinfo and it appears twice ---> grep and only take it once
- cpumodelnameCommand=$(grep -m 1 'model name' /proc/cpuinfo)
-
+ echo "$(grep -m 1 'model name' /proc/cpuinfo)" >> $memCpuLog
+ echo "$(grep -m 1 'cpu cores' /proc/cpuinfo)" >> $memCpuLog
+ cat $diskLog $memCpuLog
