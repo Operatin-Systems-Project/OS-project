df8ae1eb0a70e861118cabc0ad0a77d0d7d9f5c0
abdallah-alkanani <aa2204061@qu.edu.qa>
2024-10-30 10:39:58 +0300

    moving files around
    fixed vm2/vm3 issues

---
+++ vm3's scripts/Search.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+recipient="aa2204061@qu.edu.qa"
+output_file="bigfile"
+search_date=$(date +"%Y-%m-%d %H:%M:%S")
+results=$(find ~ -type f -size +1M 2> /dev/null)
+file_count=$(echo "$results" |wc -l )
+{
+  echo "Search Date: $search_date"
+  echo "Number of files found: $file_count"
+  echo ""
+  echo "$results"
+} > "$output_file"
+
+if [ -s "$output_file" ]; then
+  {
+    echo "To: $recipient"
+    echo "Subject: $file_count Files larger than 1MB found on VM2 on $search_date"
+    echo ""
+    echo "$results"
+  } | sendmail "$recipient" 2> /dev/null
+fi
+
