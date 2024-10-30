8d9b3b2e365d6a283b2632904d2130098bd344d9
abdallah-alkanani <aa2204061@qu.edu.qa>
2024-10-30 11:16:08 +0300

    updated clientinfo

--- vm3's scripts/Clientinfo.sh
+++ vm3's scripts/Clientinfo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-sleep 3600
+server_name="abdallah"
+server_ip="192.168.18.8"
+mkdir -p "$HOME/Desktop/logs"
+while true; do
 rm tmp 2>>/dev/null
-logfile="/home/ali/Desktop/logs/process_info_$(date +"%F_%H:%M:%S").log"
+logfile="$HOME/Desktop/logs/process_info_$(date +"%F_%H-%M-%S").log"
 echo "Process tree:" >> $logfile
 pstree >> $logfile 
 echo "Zombie processes:" >> $logfile
@@ -16,4 +18,7 @@ ps -eo pid,comm,%mem --sort=-%mem >> $logfile
 echo "Top 5 resource consumers (based on %CPU):" >> $logfile
 head -n 6 tmp >> $logfile
 rm tmp 2>>/dev/null
+scp $logfile $server_name@$server_ip:~/Desktop
 
+sleep 3600
+done
