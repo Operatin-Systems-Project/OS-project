1797e12c0fe0ec3bea0843c16d18bb37609fbd57
AmerAlzawawi <alzawawi.newten123@gmail.com>
2024-11-02 20:42:37 +0300

    update

--- vm1's scripts/traceroute.sh
+++ vm1's scripts/traceroute.sh
@@ -2,12 +2,12 @@
     echo "hostname"hostname
     echo "testing DNS server"
     LOCAL_DNS=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}' | head -n 1)
-   nlookup google.com $LOCAL_DNS
+   nslookup google.com $LOCAL_DNS
     echo "Tracing route to google.com"traceroute google.com
     ping -c 1 google.com &> /dev/null
     
 
 
 if !traceroute -m 30 $1 > /dev/null;then 
-sudoroot
+sudo reboot
 fi
