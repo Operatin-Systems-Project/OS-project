e147eb766d0a8782f48635ccb0b5e2fdc568753f
imabf <abfmashhadani@gmail.com>
2024-11-04 12:31:56 +0300

    updated traceroute.sh

--- vm2's scripts/traceroute.sh
+++ vm2's scripts/traceroute.sh
@@ -1,25 +1,21 @@
   Log="network.log"
-   logandprint() {
-    echo "$1" | tee -a $Log
-}
 
-    logandprint "routing table"
-     logandprint "$(route -n)"
-    logandprint "hostname"
-    logandprint "$(hostname)"
-    logandprint "testing DNS server"
+    echo "routing table" >> $Log
+    route -n >> $Log
+    echo "hostname" >> $Log
+    hostname >> $Log
+    echo "testing DNS server" >> $Log
     LOCAL_DNS=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}' | head -n 1)
-     logandprint "$(nslookup google.com $LOCAL_DNS)"
-    logandprint "Tracing route to google.com"
-     logandprint "$(traceroute google.com)"
-    logandprint "$(ping -c 1 google.com &> /dev/null)"
+    nslookup google.com $LOCAL_DNS >> $Log
+    echo "Tracing route to google.com" >> $Log
+    traceroute google.com >> $Log
+    ping -c 1 google.com >> $Log
     
-traceroute_output=$(traceroute -m 30 "$1" 2>&1)
-logandprint "$traceroute_output"
-last_line=$(echo "$traceroute_output" | tail -n 1)
+    traceroute -m 30 "$1" 2>&1 >> $Log
+last_line= traceroute -m 30 "$1" 2>&1  | tail -n 1 >> $Log
 if echo "$last_line" | grep -q "$1"; then
-    logandprint "Successfully traced route to $1"
+    echo "Successfully traced route to $1" >> $Log
 else
-    logandprint "Unable to reach $1 via traceroute. Rebooting..."
+    echo "Unable to reach $1 via traceroute. Rebooting..." >> $Log
     sudo reboot
 fi
