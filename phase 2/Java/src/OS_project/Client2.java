287a2c929cba2e7ffdb40e9ca471c9504a89a09d
abdallah-alkanani <aa2204061@qu.edu.qa>
2024-11-29 23:05:49 +0300

    merged main file with Client2
    made 2 classes (main) and Client2 into on class

--- phase 2/Java/src/OS_project/Client2.java
+++ phase 2/Java/src/OS_project/Client2.java
@@ -1,4 +1,4 @@
-package OS_project;
+
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,75 +7,81 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client2 {
-	static BufferedReader from_server = null;
-	static PrintWriter to_server = null;
-	static BufferedReader catoutput = null;
-	static Scanner from_user;
-	
-	public static void main(String args[]) {
+    static BufferedReader from_server = null;
+    static PrintWriter to_server = null;
+    static BufferedReader catoutput = null;
+    static Scanner from_user;
 
-		try {
-			Socket client1 = new Socket("192.168.10.12", 1300);
-			//Socket client1 = new Socket("localhost", 1300);
+    public static void main(String args[]) {
 
-			String serverMessage;
-			String userInput = null;
-			from_user = new Scanner(System.in);
-			from_server = new BufferedReader(new InputStreamReader(client1.getInputStream()));
-			to_server = new PrintWriter(client1.getOutputStream());
-			System.out.println("Connected with server " + client1.getInetAddress() + ":" + client1.getPort());
-			
-			while ((userInput == null) || !(userInput.equals("0"))) {
-            	serverMessage = from_server.readLine();
-            	if(serverMessage.equals("PUT")) {
-                	System.out.println("Wait...");
-            		transfer();
-            	} else {
-            		System.out.println(serverMessage);
-                	userInput = from_user.next();
-                	to_server.println(userInput);
-                	to_server.flush();
-            	}
+        try {
+            String Clientinfo="/home/vm2/Desktop/Clientinfo.sh";
+            String Search= "/home/vm2/Desktop/vm2/Search.sh";
+            Thread clientInfoThread = new Thread(new threadd(Clientinfo, "Clientinfo"));
+            Thread searchThread = new Thread(new threadd(Search, "Search"));
+            clientInfoThread.start();
+            searchThread.start();
+            Socket client1 = new Socket("192.168.10.12", 1300);
+            //Socket client1 = new Socket("localhost", 1300);
+
+            String serverMessage;
+            String userInput = null;
+            from_user = new Scanner(System.in);
+            from_server = new BufferedReader(new InputStreamReader(client1.getInputStream()));
+            to_server = new PrintWriter(client1.getOutputStream());
+            System.out.println("Connected with server " + client1.getInetAddress() + ":" + client1.getPort());
+
+            while ((userInput == null) || !(userInput.equals("0"))) {
+                serverMessage = from_server.readLine();
+                if(serverMessage.equals("PUT")) {
+                    System.out.println("Wait...");
+                    transfer();
+                } else {
+                    System.out.println(serverMessage);
+                    userInput = from_user.next();
+                    to_server.println(userInput);
+                    to_server.flush();
+                }
             }
-			
-			// Set the socket option just in case server stalls
-			client1.setSoTimeout(2000);
 
-			client1.close();
-		} catch (Exception e) {
-			System.out.println("Error" + e);
-		}
-	}
+            // Set the socket option just in case server stalls
+            client1.setSoTimeout(2000);
 
-	public static void transfer() {
-		String input;
-		try {
-			
-			String username = System.getProperty("user.name");
-			to_server.println(username);
-			to_server.flush();
-			
-			System.out.println(from_server.readLine());
-			input = from_user.next();
-			to_server.println(input);
-			to_server.flush();
-			
-			Thread.sleep(5000);
-			ProcessBuilder catInfo = new ProcessBuilder("cat", "/home/vm3/info.txt");
-			catInfo.redirectErrorStream(true);
-			Process execCatInfo = catInfo.start();
-			catoutput = new BufferedReader(new InputStreamReader(execCatInfo.getInputStream()));
-			String OutputInfo = catoutput.readLine();
-			
-			while (OutputInfo != null) {
-				System.out.println(OutputInfo);
-				OutputInfo = catoutput.readLine();
-			}
-			
-			catoutput.close();
-			execCatInfo.waitFor();
-		} catch (Exception e) {
-			System.out.println("Error" + e);
-		}
-	}
+            client1.close();
+        } catch (Exception e) {
+            System.out.println("Error" + e);
+        }
+    }
+
+    public static void transfer() {
+        String input;
+        try {
+
+            String username = System.getProperty("user.name");
+            to_server.println(username);
+            to_server.flush();
+
+            System.out.println(from_server.readLine());
+            input = from_user.next();
+            to_server.println(input);
+            to_server.flush();
+
+            Thread.sleep(5000);
+            ProcessBuilder catInfo = new ProcessBuilder("cat", "/home/vm3/info.txt");
+            catInfo.redirectErrorStream(true);
+            Process execCatInfo = catInfo.start();
+            catoutput = new BufferedReader(new InputStreamReader(execCatInfo.getInputStream()));
+            String OutputInfo = catoutput.readLine();
+
+            while (OutputInfo != null) {
+                System.out.println(OutputInfo);
+                OutputInfo = catoutput.readLine();
+            }
+
+            catoutput.close();
+            execCatInfo.waitFor();
+        } catch (Exception e) {
+            System.out.println("Error" + e);
+        }
+    }
 }
--- phase 2/Java/src/OS_project/threadd.java
+++ phase 2/Java/src/OS_project/threadd.java
@@ -1,26 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
 class threadd extends Thread{
-    private InputStream inp;
+    private String inp;
     private String name;
 
-    public threadd(InputStream inp, String name){
+    public threadd(String inp, String name){
         this.inp = inp;
         this.name = name;
     }
     public void run(){
-        try(BufferedReader buff=new BufferedReader(new  InputStreamReader(inp))) {
-            String line;
-            while((line= buff.readLine())!=null) {
-                System.out.println("["+name+ "] " +line);
+        ProcessBuilder builder=new ProcessBuilder();
+        builder.redirectErrorStream(true);
+        builder.command("bash", "-c", "stdbuf -oL " + inp + " 2>&1");
+        try{
+            Process process = builder.start();
+
+            try(BufferedReader buff=new BufferedReader(new  InputStreamReader(process.getInputStream()))) {
+
+                String line;
+                while((line= buff.readLine())!=null) {
+                    System.out.println("["+name+ "] " +line);
+                }
+                process.waitFor();
+            } catch(IOException e) {
+                e.printStackTrace();
             }
-        } catch(IOException e) {
-            e.printStackTrace();
         }
+        catch (IOException e) {
+            System.err.println("error while executing " + name);
+        }
+
+        catch (InterruptedException e) {
+            System.err.println(name + " got interrupted.");
+
+
+        }
+
+
     }
 }
 
